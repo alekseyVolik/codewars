03896dd1fb7c19de18bc88d6bcc6fa2ca56af20f
smalltownboy <44425124+alekseyVolik@users.noreply.github.com>
2021-01-29 15:20:42 +0400

    Delete balanced_number.py

--- balanced_number.py
+++
@@ -1,7 +0,0 @@
-# Task: https://www.codewars.com/kata/5a4e3782880385ba68000018/train/python
-def balanced_num(number):
-    number = list(map(int, str(number)))
-    middle = (len(number)-1)
-    if middle > 1:
-        return "Balanced" if (sum(number[:middle//2]) == sum(number[round(middle/2) + 1:])) else "Not Balanced"
-    return "Balanced"
