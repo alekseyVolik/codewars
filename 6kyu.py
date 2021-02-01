e9fbc55be627bb6c59769b44b6cad3459f2c0e63
smalltownboy <volikjob@gmail.com>
2021-02-01 14:53:57 +0400

    Update 6kyu.py
    Added solve for tasks:
    1) https://www.codewars.com/kata/5208f99aee097e6552000148/train/python

---
+++ 6kyu.py
@@ -0,0 +1,11 @@
+def break_camel_casing(text):
+    """
+    Break up camel casing string with space between words
+
+    link: https://www.codewars.com/kata/5208f99aee097e6552000148/train/python
+    """
+    return ''.join([' ' + letter if letter.isupper() else letter for letter in text])
+
+
+if __name__ == '__main__':
+    print(break_camel_casing('breakCamelCase'))
