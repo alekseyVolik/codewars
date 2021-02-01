93ae89c021af1161c0b7d8be38726f5cb6a1a3d3
smalltownboy <volikjob@gmail.com>
2021-02-01 14:54:42 +0400

    Update 7kyu.py

--- 7kyu.py
+++ 7kyu.py
@@ -7,5 +7,18 @@ def hit_the_target(matrix):
     return any([('>' in row) and ('x' in row) and (row.index('>') < row.index('x')) for row in matrix])
 
 
+def alphabet_war(fight):
+    """
+    Return winner side of fight
+
+    Link: https://www.codewars.com/kata/59377c53e66267c8f6000027/train/python
+    """
+    ls = ['s', 'b', 'p', 'w']
+    rs = ['z', 'd', 'q', 'm']
+    lp = sum([pos * fight.count(letter) for (pos, letter) in enumerate(ls, 1)])
+    rp = sum([pos * fight.count(letter) for (pos, letter) in enumerate(rs, 1)])
+    return "Right side wins!" if rp > lp else ("Left side wins!" if rp < lp else "Let's fight again!")
+
+
 if __name__ == '__main__':
     pass
