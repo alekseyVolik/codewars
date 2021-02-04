b8fd6d681fa954520401d9c03b0d00d4ed9d62ea
smalltownboy <volikjob@gmail.com>
2021-02-04 11:27:01 +0400

    Update 6kyu.py
    Added solutions for following tasks:
    1) https://www.codewars.com/kata/55b0fb65e1227b17d60000dc/train/python

--- 6kyu.py
+++ 6kyu.py
@@ -7,5 +7,24 @@ def break_camel_casing(text):
     return ''.join([' ' + letter if letter.isupper() else letter for letter in text])
 
 
+def json_attr(filepath):
+    """
+    Set attribute from json to class
+
+    link: https://www.codewars.com/kata/55b0fb65e1227b17d60000dc/train/python
+    """
+    import json
+    json_file = open(filepath, 'r')
+    json_dict = json.load(json_file)
+    json_file.close()
+
+    def set_attr(some_class):
+        for attr, value in json_dict.items():
+            setattr(some_class, attr, value)
+        return some_class
+
+    return set_attr
+
+
 if __name__ == '__main__':
     pass
